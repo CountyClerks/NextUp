4ea2f7d0185d3e087b6c9bdc3a9f97e9a7ab8daa
Eric Hardy <erichardy011@gmail.com>
2025-09-26 18:02:16 -0500

    Components file structure

---
+++ src/components/Menu.tsx
@@ -0,0 +1,19 @@
+export default function Menu() {
+    return( 
+        <>
+            <section className="menu-container">
+                <div className="menu-container-banner">
+                    <h1>Menu</h1>
+                </div>
+                <div className="menu-container-tastks">
+                    <h2>Tasks</h2>
+                        <h3>Today</h3>
+                        <h3>Calendar</h3>
+                </div>'
+                <div className="menu-container-lists">
+                    <h2>Lists</h2>
+                </div>
+            </section>
+        </>
+    )
+}
