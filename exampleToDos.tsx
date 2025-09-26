cfae04e67db3eb093412e03690d2f7b9a3c50cf7
Eric Hardy <erichardy011@gmail.com>
2025-09-26 17:59:54 -0500

    Example ToDo tasks from json to tsx objekt

---
+++ exampleToDos.tsx
@@ -0,0 +1,23 @@
+const todos = [
+    {
+        "id": "1",
+        "title": "Buy groceries",
+        "description": "Milk, eggs, bread, and vegetables.",
+        "completed": false,
+        "group": "Grocery"
+    },
+    {
+        "id": "2",
+        "title": "Finish report",
+        "description": "Complete the quarterly sales report for Q3.",
+        "completed": false,
+        "group": "Work"
+    },
+    {
+        "id": "3",
+        "title": "Call mechanic",
+        "description": "Schedule car maintenance.",
+        "completed": true,
+        "group": "Life"
+    }
+]
