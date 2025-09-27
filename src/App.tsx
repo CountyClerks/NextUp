3f6a1f00e61386a21725eaa4b1d0fde97d410cf6
Eric Hardy <erichardy011@gmail.com>
2025-09-26 22:56:16 -0500

    switch to development utilizing branches

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import Menu from "./components/Menu"
 
 function App() {
 
   return (
     <>
+      <Menu />
     </>
   )
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,17 +1,34 @@
+import todos from "../../exampleToDo.json"
+import "./Menu.css"
+import { useState } from "react"
 export default function Menu() {
+
+    const [todoGroups, setToDoGroups] = useState(todos)
+
+    const listGroups = todoGroups.map((todos, index) => {
+        return (
+            <div className="menu-container-lists-category-item" key={`${todos.group}-${index}`}>
+                <h3>{todos.group}</h3>
+            </div>
+        )
+    })
+
     return( 
         <>
             <section className="menu-container">
                 <div className="menu-container-banner">
                     <h1>Menu</h1>
+                    <button className="menu-burger"/>
                 </div>
-                <div className="menu-container-tastks">
+                <div className="menu-container-tasks">
                     <h2>Tasks</h2>
+                        <h3>Upcoming</h3>
                         <h3>Today</h3>
                         <h3>Calendar</h3>
-                </div>'
+                </div>
                 <div className="menu-container-lists">
                     <h2>Lists</h2>
+                    <div className="menu-container-lists-category">{listGroups}</div>
                 </div>
             </section>
         </>
